761969ea4cc711252b7637c889005aba59157913
MDThomsen <mdahlthomsen@hotmail.com>
2019-04-06 18:15:59 +0200

    models update, added max length and uniqueness to session.code

--- circle-backend/circle-backend/Database/Models/Session.cs
+++ circle-backend/circle-backend/Database/Models/Session.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace circle_backend.Models
 {
 	public class Session
     {
+        [Key]
         public int SessionId { get; set; }
-        public ICollection<Message> Messages { get; set; }
+        public string Code { get; set; }
+        public bool HasStarted { get; set; }
+        public ICollection<DrawingMessage> DrawingMessages { get; set; }
+        public ICollection<TextMessage> TextMessages { get; set; }
     }
 }
