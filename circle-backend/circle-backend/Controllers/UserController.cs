64630aad911931bf8bd61e51ce38d31bfc30791f
David Tran <davidtran19@yahoo.com>
2019-04-06 21:43:27 +0200

    User repository and controller works

---
+++ circle-backend/circle-backend/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using circle_backend.Database.Repositories;
+using circle_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace circle_backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+            
+        [HttpPut]
+        public void AddUser([FromBody]User user) 
+        {
+            _userRepository.AddUser(user);
+            _userRepository.Save();
+        }
+
+        [HttpGet("{userId}")]
+        public User GetUserById(int userId)
+        {
+            User user = _userRepository.GetUserById(userId);
+            return user;
+        }
+
+        [HttpPost("{userId}/{readyStatus}")]
+        public void SetUserReadyStatus(int userId, bool readyStatus)
+        {
+            _userRepository.SetUserReadyStatus(userId, readyStatus);
+            _userRepository.Save();
+        }
+
+        [HttpPost("{userId}/{turnCompleteStatus}")]
+        public void SetUserCompleteStatus(int userId, bool turnCompleteStatus)
+        {
+            _userRepository.SetUserReadyStatus(userId, turnCompleteStatus);
+            _userRepository.Save();
+        }
+
+        [HttpDelete("{userId}")]
+        public void RemoveUser(int userId) 
+        {
+            _userRepository.RemoveUser(userId);
+            _userRepository.Save();
+        } 
+
+    }
+}
---
+++ circle-backend/circle-backend/Database/Repositories/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using circle_backend.Models;
+
+namespace circle_backend.Database.Repositories
+{
+    public class UserRepository : IUserRepository, IDisposable
+    {
+        private readonly CircleDbContext _context;
+
+        public UserRepository(CircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public User GetUserById(int userId) 
+        {
+            User user = _context.Users.Find(userId);
+            return user;
+        }
+
+        public void SetUserReadyStatus(int userId, bool readyStatus) 
+        {
+            User user = _context.Users.Find(userId);
+            user.Ready = readyStatus;
+        }
+
+        public void SetUserCompleteStatus(int userId, bool turnCompleteStatus)
+        {
+            User user = _context.Users.Find(userId);
+            user.TurnComplete = turnCompleteStatus;
+        }
+
+        public void RemoveUser(int userId)
+        {
+            User user = _context.Users.Find(userId);
+            _context.Users.Remove(user);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
